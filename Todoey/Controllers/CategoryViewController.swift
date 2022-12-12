89728fa18129f4e6078f154cf5e0401243c450a3
MelihYuvaci <94677369+MelihYuvaci@users.noreply.github.com>
2022-12-12 16:36:52 +0300

    Add Categories and Update Database Relationships

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,79 @@
+//
+//  CategoryTableViewController.swift
+//  Todoey
+//
+//  Created by Melih Yuvacı on 12.12.2022.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    var categories = [Category]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadCategories()
+    }
+    
+    //MARK: - TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        let category = categories[indexPath.row]
+        cell.textLabel?.text = category.name
+        return cell
+    }
+    
+    //MARK: - Add New Categories
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add", style: .default){ action in
+            let newCategory = Category(context: self.context)
+            newCategory.name = textField.text!
+            self.categories.append(newCategory)
+            self.saveCategories()
+        }
+        
+        alert.addTextField{ alertTextField in
+            alertTextField.placeholder = "Add a new category"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+    
+    //MARK: - Data Manipulation Methods
+    
+    func saveCategories(){
+        do{
+           try context.save()
+        }catch{
+            print("Error saving context \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    
+    func loadCategories(with request:NSFetchRequest<Category> =  Category.fetchRequest()){
+        do {
+           categories = try context.fetch(request)
+        }catch{
+            print("Error fetching request \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    //MARK: - TableView Delegate Methods
+    
+    
+    
+
+}
