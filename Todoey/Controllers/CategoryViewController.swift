58d4e6befd14a7ff84321a9d418a236db8a8c8e6
MelihYuvaci <94677369+MelihYuvaci@users.noreply.github.com>
2022-12-16 16:49:40 +0300

    Save Data using Realm

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -7,17 +7,17 @@
 //
 
 import UIKit
-import CoreData
+import RealmSwift
 
 class CategoryViewController: UITableViewController {
-
-    var categories = [Category]()
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-
+    
+    let realm = try! Realm()
+    var categories : Results<Category>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
-        loadCategories()
+        //        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+//        loadCategories()
     }
     
     //MARK: - TableView Datasource Methods
@@ -48,47 +48,45 @@ class CategoryViewController: UITableViewController {
     
     
     //MARK: - Add New Categories
-
+    
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         var textField = UITextField()
         let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add", style: .default){ action in
-            let newCategory = Category(context: self.context)
+            let newCategory = Category()
             newCategory.name = textField.text!
-            self.categories.append(newCategory)
-            self.saveCategories()
+            self.save(category: newCategory)
         }
-        
+        alert.addAction(action)
         alert.addTextField{ alertTextField in
             alertTextField.placeholder = "Add a new category"
             textField = alertTextField
         }
-        alert.addAction(action)
+        
         present(alert, animated: true)
     }
     
     //MARK: - Data Manipulation Methods
     
-    func saveCategories(){
+    func save(category: Category){
         do{
-           try context.save()
+            try realm.write{
+                realm.add(category)
+            }
         }catch{
             print("Error saving context \(error)")
         }
         self.tableView.reloadData()
     }
     
-    func loadCategories(with request:NSFetchRequest<Category> =  Category.fetchRequest()){
-        do {
-           categories = try context.fetch(request)
-        }catch{
-            print("Error fetching request \(error)")
-        }
-        self.tableView.reloadData()
-    }
+//    func loadCategories(){
+//        categories = realm.objects(Category.self)
+//
+//
+//        self.tableView.reloadData()
+//    }
     
     
     
     
-
 }
--- Todoey/DataModel/Category.swift
+++ Todoey/DataModel/Category.swift
@@ -1,5 +1,5 @@
 //
-//  Data.swift
+//  Category.swift
 //  Todoey
 //
 //  Created by Melih Yuvacı on 13.12.2022.
@@ -9,7 +9,8 @@
 import Foundation
 import RealmSwift
 
-class Data:Object{
-   @objc dynamic var name: String = ""
-   @objc dynamic var age: Int = 0
+class Category : Object {
+    @objc dynamic var name : String = ""
+    let items = List<Item>()
+    
 }
